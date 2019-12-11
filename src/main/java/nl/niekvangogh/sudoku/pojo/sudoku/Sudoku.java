bfccfb86a973ffcc2e8f04e68d33b262aeda9ddb
Niek van Gogh <ik@niekvangogh.nl>
2019-12-11 12:09:39 +0100

    adding implementation

--- src/main/java/nl/niekvangogh/sudoku/pojo/sudoku/Sudoku.java
+++ src/main/java/nl/niekvangogh/sudoku/pojo/sudoku/Sudoku.java
@@ -2,12 +2,31 @@ package nl.niekvangogh.sudoku.pojo.sudoku;
 
 import lombok.Getter;
 
+import java.util.Random;
+
 public class Sudoku {
+
+    private final Random randomGenerator;
 
     @Getter
     private Tile[][] grid;
 
-    public Sudoku() {
+    public Sudoku(long seed) {
+        this.randomGenerator = new Random(seed);
         this.grid = new Tile[9][9];
+        for (int x = 0; x < 9; x++) {
+            for (int y = 0; y < 9; y++) {
+                this.grid[x][y] = new Tile(x, y, 0);
+            }
+        }
     }
+
+    public Sudoku() {
+        this(new Random().nextLong());
+    }
+
+    public int getRandomGenerator(int num) {
+        return (int) Math.floor((this.randomGenerator.nextDouble() * num + 1));
+    }
+
 }
--- src/main/java/nl/niekvangogh/sudoku/service/impl/SudokuServiceImpl.java
+++ src/main/java/nl/niekvangogh/sudoku/service/impl/SudokuServiceImpl.java
@@ -5,36 +5,69 @@ import nl.niekvangogh.sudoku.pojo.sudoku.Sudoku;
 import nl.niekvangogh.sudoku.pojo.sudoku.Tile;
 import nl.niekvangogh.sudoku.service.SudokuService;
 
+import java.util.Arrays;
+import java.util.Random;
+
 public class SudokuServiceImpl implements SudokuService {
 
     @Override
     public Sudoku generateSudoku() {
-        return null;
+        Sudoku sudoku = new Sudoku();
+        this.fillDiagonal(sudoku);
+        this.fillSudoku(sudoku);
+        return sudoku;
     }
 
     @Override
     public void fillDiagonal(Sudoku sudoku) {
-
+        for (int i = 0; i < 9; i = i + 3) {
+            System.out.println(i);
+            this.fillBox(sudoku, this.getTile(sudoku, i, i));
+        }
     }
 
     @Override
     public void fillBox(Sudoku sudoku, Tile tile) {
+        int num;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                do {
+                    num = sudoku.getRandomGenerator(9);
+                }
+                while (!this.isUnusedInBox(sudoku, tile, num));
+
+                Tile[] tiles = this.getBox(sudoku, tile.getXPos(), tile.getYPos());
+                // requires refactoring
+                sudoku.getGrid()[tiles[0].getXPos() + i][tiles[0].getYPos() + j].setSolution(num);
+            }
+        }
 
     }
 
     @Override
-    public Tile[] getBox(Sudoku sudoku, int x, int y) {
-        return new Tile[0];
+    public Tile[] getBox(Sudoku sudoku, int xPos, int yPos) {
+        int startX = (xPos / 3) * 3;
+        int startY = (yPos / 3) * 3;
+
+        Tile[] tiles = new Tile[9];
+        for (int x = 0; x < 3; x++) {
+            for (int y = 0; y < 3; y++) {
+                tiles[(3 * y) + x] = this.getTile(sudoku, startX + x, startY + y);
+            }
+        }
+        return tiles;
     }
 
     @Override
     public boolean checkIfSafe(Sudoku sudoku, Tile tile, int value) {
-        return false;
+// lots of work cba
+        return true;
     }
 
     @Override
     public boolean isUnusedInBox(Sudoku sudoku, Tile tile, int value) {
-        return false;
+        Tile[] box = this.getBox(sudoku, tile.getXPos(), tile.getYPos());
+        return Arrays.stream(box).noneMatch(boxTile -> boxTile.getSolution() == value);
     }
 
     @Override
@@ -59,6 +92,6 @@ public class SudokuServiceImpl implements SudokuService {
 
     @Override
     public Tile getTile(Sudoku sudoku, int x, int y) {
-        return null;
+        return sudoku.getGrid()[x][y];
     }
 }
