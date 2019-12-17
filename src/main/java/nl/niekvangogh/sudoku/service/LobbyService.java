a6df723d36d48b78b02b5e3fc88c867e2a8812b3
Niek van Gogh <ik@niekvangogh.nl>
2019-12-17 10:27:45 +0100

    added lobby interface

--- src/main/java/nl/niekvangogh/sudoku/service/LobbyService.java
+++ src/main/java/nl/niekvangogh/sudoku/service/LobbyService.java
@@ -1,13 +1,18 @@
 package nl.niekvangogh.sudoku.service;
 
+import nl.niekvangogh.sudoku.entity.ChatMessage;
 import nl.niekvangogh.sudoku.entity.Player;
 import nl.niekvangogh.sudoku.pojo.Ranking;
 import nl.niekvangogh.sudoku.pojo.lobby.Lobby;
 
 public interface LobbyService {
 
+    Lobby createLobby();
+
     Lobby findLobby(Player player);
 
     void joinLobby(Lobby lobby, Player player);
+
+    void onChat(Player player, ChatMessage message);
 
 }
--- src/main/java/nl/niekvangogh/sudoku/service/impl/LobbyServiceImpl.java
+++ src/main/java/nl/niekvangogh/sudoku/service/impl/LobbyServiceImpl.java
@@ -1,6 +1,24 @@
 package nl.niekvangogh.sudoku.service.impl;
 
+import nl.niekvangogh.sudoku.entity.ChatMessage;
+import nl.niekvangogh.sudoku.entity.Player;
+import nl.niekvangogh.sudoku.pojo.lobby.Lobby;
 import nl.niekvangogh.sudoku.service.LobbyService;
 
-public abstract class LobbyServiceImpl implements LobbyService {
+public class LobbyServiceImpl implements LobbyService {
+
+    @Override
+    public Lobby findLobby(Player player) {
+        return null;
+    }
+
+    @Override
+    public void joinLobby(Lobby lobby, Player player) {
+
+    }
+
+    @Override
+    public void onChat(Player player, ChatMessage message) {
+
+    }
 }
