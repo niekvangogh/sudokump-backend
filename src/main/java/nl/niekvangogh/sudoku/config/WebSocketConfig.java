030775755a228997dfe1906301d9e17f7a208cea
Niek van Gogh <ik@niekvangogh.nl>
2019-12-18 14:13:54 +0100

    YESSS

--- src/main/java/nl/niekvangogh/sudoku/config/WebSocketConfig.java
+++ src/main/java/nl/niekvangogh/sudoku/config/WebSocketConfig.java
@@ -46,6 +46,6 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
                         }
                         return true;
                     }
-                }).withSockJS();
+                });
     }
 }
