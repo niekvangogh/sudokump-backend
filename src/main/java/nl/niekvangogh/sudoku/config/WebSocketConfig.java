29d7627df707ce99ed9fd6e5e076868e85f4e7b8
Niek <ik@niekvangogh.nl>
2020-01-04 19:59:26 +0100

    backup commit

--- src/main/java/nl/niekvangogh/sudoku/config/WebSocketConfig.java
+++ src/main/java/nl/niekvangogh/sudoku/config/WebSocketConfig.java
@@ -1,52 +1,53 @@
 package nl.niekvangogh.sudoku.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.server.ServletServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpRequest;
-import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
+import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
-import javax.servlet.http.HttpSession;
-import java.util.Map;
+import java.lang.invoke.MethodHandles;
+
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
 
-    @Override
-    public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic/", "/queue");
-        config.setApplicationDestinationPrefixes("/app", "/queue", "/game");
-    }
+    private static Logger log = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-
-        registry
-                .addEndpoint("/greeting")
+        registry.addEndpoint("/ws")
+                .setHandshakeHandler(new AssignPrincipalHandshakeHandler())
                 .setAllowedOrigins("*")
                 .setAllowedOrigins("http://localhost:3000")
-                .setHandshakeHandler(new DefaultHandshakeHandler() {
-                    public boolean beforeHandshake(
-                            ServerHttpRequest request,
-                            ServerHttpResponse response,
-                            WebSocketHandler wsHandler,
-                            Map attributes) throws Exception {
+                .withSockJS();
+    }
 
-                        if (request instanceof ServletServerHttpRequest) {
-                            ServletServerHttpRequest servletRequest
-                                    = (ServletServerHttpRequest) request;
-                            HttpSession session = servletRequest
-                                    .getServletRequest().getSession();
-                            attributes.put("sessionId", session.getId());
-                        }
-                        return true;
-                    }
-                }).withSockJS();
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry registry) {
+        registry.setApplicationDestinationPrefixes("/app");
+        registry.enableSimpleBroker("/queue", "/game");
+    }
+
+    @EventListener
+    public void handleSubscribeEvent(SessionSubscribeEvent event) {
+        log.info("<==> handleSubscribeEvent: username=" + event.getUser().getName() + ", event=" + event);
+    }
+
+    @EventListener
+    public void handleConnectEvent(SessionConnectEvent event) {
+        log.info("===> handleConnectEvent: username=" + event.getUser().getName() + ", event=" + event);
+    }
+
+    @EventListener
+    public void handleDisconnectEvent(SessionDisconnectEvent event) {
+        log.info("<=== handleDisconnectEvent: username=" + event.getUser().getName() + ", event=" + event);
     }
 }
--- src/main/java/nl/niekvangogh/sudoku/controller/GameController.java
+++ src/main/java/nl/niekvangogh/sudoku/controller/GameController.java
@@ -1,34 +1,50 @@
 package nl.niekvangogh.sudoku.controller;
 
 import com.google.gson.Gson;
+import nl.niekvangogh.sudoku.entity.User;
+import nl.niekvangogh.sudoku.service.impl.GameManagerServiceImpl;
 import nl.niekvangogh.sudoku.service.impl.GameServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageHeaders;
 import org.springframework.messaging.handler.annotation.MessageExceptionHandler;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import org.springframework.messaging.simp.SimpMessageType;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 
 import java.security.Principal;
 import java.util.Map;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @Controller()
 public class GameController {
 
     @Autowired
-    private SimpMessageSendingOperations messagingTemplate;
+    private SimpMessageSendingOperations messageSendingService;
 
-    private Gson gson = new Gson();
+    @Autowired
+    private GameManagerServiceImpl gameManagerService;
 
-    @MessageMapping("/queue")
-    @SendTo("/queue/status")
-    public String processMessageFromClient(
-            @Payload String message,
-            Principal principal) throws Exception {
-        return " yes";
+    @MessageMapping("/queue/start")
+    public void startQueue(Message<Object> message, @Payload String payload, Principal principal, SimpMessageHeaderAccessor accessor) throws Exception {
+        this.gameManagerService.queuePlayer(new User());
+
+        System.out.println(principal.getName());
+
+
+        this.messageSendingService.convertAndSendToUser(principal.getName(), "/queue/status", "test1");
+        this.messageSendingService.convertAndSendToUser(accessor.getSessionId(), "/queue/status", "test3");
+    }
+
+    @MessageMapping("/queue/cancel")
+    public void cancelQueue() {
+//        this.gameManagerService.cancelQueue(player);
     }
 
     @MessageExceptionHandler
