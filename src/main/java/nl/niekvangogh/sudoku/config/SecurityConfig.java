e788844073dad43538543d9d74fb9761786853ac
Niek <ik@niekvangogh.nl>
2020-01-04 21:54:08 +0100

    lol no copy pasta

--- src/main/java/nl/niekvangogh/sudoku/config/SecurityConfig.java
+++ src/main/java/nl/niekvangogh/sudoku/config/SecurityConfig.java
@@ -82,6 +82,10 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
+//
+//        .csrf().disable()
+//                .authorizeRequests()
+//                .antMatchers("/**").permitAll();
         http
                 .cors()
                 .and()
